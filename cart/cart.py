0f1ed634a2c10203ce8c0fd68c7ea78eb0d7f737
Angel Velasquez <angvp@archlinux.org>
2013-06-06 11:22:09 -0300

    Reverting to 23d71b7cc6e8e369a06246c204975ac2b861f80c

---
+++ cart/cart.py
@@ -0,0 +1,102 @@
+from django.contrib.auth.models import AnonymousUser
+
+import datetime
+import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+
+CART_ID = 'CART-ID'
+
+
+class ItemAlreadyExists(Exception):
+    pass
+
+
+class ItemDoesNotExist(Exception):
+    pass
+
+
+class Cart:
+    def __init__(self, request):
+        user = request.user
+        #First search by user
+        if not user.is_anonymous():
+            try:
+                cart = models.Cart.objects.get(user=user)
+            except models.Cart.DoesNotExist:
+                cart = self.new(request)
+            self.cart = cart
+        #If not, search by request id
+        else:
+            cart_id = request.session.get(CART_ID)
+            if cart_id:
+                try:
+                    cart = models.Cart.objects.get(id=cart_id, checked_out=False)
+                except models.Cart.DoesNotExist:
+                    cart = self.new(request)
+            else:
+                cart = self.new(request)
+            self.cart = cart
+
+    def __iter__(self):
+        for item in self.cart.item_set.all():
+            yield item
+
+    @classmethod
+    def getCart(self, request):
+        cart_id = request.session.get(CART_ID)
+        if cart_id:
+            cart = models.Cart.objects.get(id=cart_id, checked_out=False)
+        else:
+            cart = None
+        return cart
+
+    def new(self, request):
+        cart = models.Cart(creation_date=datetime.datetime.now())
+        cart.save()
+        request.session[CART_ID] = cart.id
+        return cart
+
+    def add(self, product, unit_price, quantity=1):
+        try:
+            item = models.Item.objects.get(
+                cart=self.cart,
+                product=product,
+            )
+        except models.Item.DoesNotExist:
+            item = models.Item()
+            item.cart = self.cart
+            item.product = product
+            item.unit_price = unit_price
+            item.quantity = quantity
+            item.save()
+        else:
+            item.quantity += quantity
+            item.save()
+
+    def remove(self, product):
+        try:
+            item = models.Item.objects.get(
+                cart=self.cart,
+                product=product,
+            )
+        except models.Item.DoesNotExist:
+            raise ItemDoesNotExist
+        else:
+            item.delete()
+
+    def update(self, product, quantity, unit_price=None):
+        try:
+            item = models.Item.objects.get(
+                cart=self.cart,
+                product=product,
+            )
+            item.quantity = quantity
+            item.save()
+        except models.Item.DoesNotExist:
+            raise ItemDoesNotExist
+
+    def clear(self):
+        for item in self.cart.item_set:
+            item.delete()
