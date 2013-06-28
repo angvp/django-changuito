7dfea1a7ffa1fdd5d89aa0f25e3a1e801d8ca49e
Angel Velasquez <angvp@archlinux.org>
2013-06-28 16:47:48 -0300

    Fixing bug when user login

--- changuito/signals.py
+++ changuito/signals.py
@@ -6,9 +6,8 @@ def merge_cart(sender, user, request, **kwargs):
     # lets overwrite the previous cart in case the user had a car
     if not cart.is_empty():
         cart.replace(cart.cart.id, user)
-        return True
-
-    cart.__init__(request)
+    else:
+        cart.__init__(request)
 
 
 user_logged_in.connect(merge_cart)
