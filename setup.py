631183979f620f653f4a5d5fcd879f6562143539
Angel Velasquez <angvp@archlinux.org>
2013-06-30 23:23:38 -0300

    Preparing for the 0.1 release

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ setup(
             "Environment :: Web Environment",
             "Framework :: Django",
             "Intended Audience :: Developers",
-            "License :: OSI Approved :: LGPL",
             "Operating System :: OS Independent",
             "Programming Language :: Python",
             "Topic :: Software Development :: Libraries :: Python Modules",
