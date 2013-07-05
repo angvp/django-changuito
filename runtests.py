70516f5957ad698766e0e4c819ff8d6e102f1cda
Angel Velasquez <angvp@archlinux.org>
2013-07-04 21:26:42 -0300

    Fix travis and tests

---
+++ runtests.py
@@ -0,0 +1,28 @@
+import sys
+
+from django.conf import settings
+
+settings.configure(
+    DEBUG=True,
+    USE_TZ=True,
+    DATABASES={
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+        }
+    },
+    INSTALLED_APPS=[
+        "django.contrib.auth",
+        "django.contrib.contenttypes",
+        "django.contrib.sites",
+        "changuito",
+    ],
+    SITE_ID=1,
+)
+
+from django_nose import NoseTestSuiteRunner
+
+test_runner = NoseTestSuiteRunner(verbosity=1)
+failures = test_runner.run_tests(["changuito"])
+
+if failures:
+    sys.exit(failures)
