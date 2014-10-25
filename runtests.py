f317e5fed9f2e2c1f63c90e3d90242ce0611d52f
Angel Velasquez <angvp@archlinux.org>
2014-10-25 01:43:49 -0300

    Remove nose (overkill for now) and use django.setup if Django == 1.7

--- runtests.py
+++ runtests.py
@@ -2,30 +2,40 @@ import sys
 
 from django.conf import settings
 
-settings.configure(
-    DEBUG=True,
-    USE_TZ=True,
-    DATABASES={
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": ":memory:",
-        }
-    },
-    INSTALLED_APPS=[
-        "django.contrib.auth",
-        "django.contrib.contenttypes",
-        "django.contrib.sites",
-        "changuito",
-    ],
-    SITE_ID=1,
-)
+if not settings.configured:
+    settings.configure(
+        DEBUG=True,
+        USE_TZ=True,
+        DATABASES={
+            "default": {
+                "ENGINE": "django.db.backends.sqlite3",
+                "NAME": ":memory:",
+            }
+        },
+        INSTALLED_APPS=[
+            "django.contrib.auth",
+            "django.contrib.contenttypes",
+            "django.contrib.sites",
+            "changuito",
+        ],
+        SITE_ID=1,
+        MIDDLEWARE_CLASSES=[],
+    )
 
-from django_nose import NoseTestSuiteRunner
+
 from django.test.utils import get_runner
+
+# Django 1.7 fix for tests
+try:
+    from django import setup
+    setup()
+except ImportError:
+    pass
 
 
 def runtests():
-    test_runner = NoseTestSuiteRunner(verbosity=1, interactive=True, failfast=False)
+    TestRunner = get_runner(settings)
+    test_runner = TestRunner(verbosity=1, interactive=True, failfast=False)
     failures = test_runner.run_tests(["changuito"])
     sys.exit(failures)
 
