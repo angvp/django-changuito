b6336a5c30492bcfbacc2ddb22561619e19cb503
Angel Velasquez <angvp@archlinux.org>
2020-01-14 17:17:33 -0500

    Modernize and prepare for the future version

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='django-changuito',
-    version='1.2',
+    version='2.0',
     description='A fork of django-cart with the same simplicity but updated',
     maintainer='Angel Velasquez',
     maintainer_email='angvp@archlinux.org',
     license="LGPL v3",
     url='https://github.com/angvp/django-changuito',
-    packages=['changuito'],
+    packages=find_packages(),
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Environment :: Web Environment",
